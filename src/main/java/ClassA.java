5335e678b69d833d1f8bdd755bcf3840cf7603d0
Vika <viktoria.bozhko@jetbrains.com>
2021-09-22 15:33:51 +0300

    -1-

---
+++ src/main/java/ClassA.java
@@ -0,0 +1,16 @@
+public class ClassA {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("olololo");
+        }
+    }
+
+    public void foo() {
+        return;
+    }
+
+    public String str(String s) {
+        return s;
+    }
+}
